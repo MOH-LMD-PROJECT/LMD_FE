78c76177ead78c6e07c7a699d0070c8a6c45f3dc
Bkroland19 <bkroland19@gmail.com>
2023-06-28 15:56:43 +0300

    auth

---
+++ src/api/loginApi.ts
@@ -0,0 +1,20 @@
+import { apiClient } from './apiClient';
+
+export const login = async ({ username, password }:any) => {
+  const response = await apiClient.post('/login', {
+    method: 'POST',
+    body: { username, password },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  // if (!response.ok) {
+  //   throw new Error('Login failed');
+  // }
+
+  // Assuming the server responds with a JSON object containing the user token
+
+
+  return response;
+};
