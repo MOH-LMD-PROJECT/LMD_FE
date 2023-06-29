44a67c7881acefa46a3618e5d489fec21cc8baf4
engWafula <wafulaallan5@gmail.com>
2023-06-29 17:13:16 +0300

    adding changes

--- src/pages/MOH/AdminDashboard/index.tsx
+++ src/pages/MOH/AdminDashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ChartOne from '../../../components/ChartOne';
 import ChartThree from '../../../components/ChartThree';
 import ChartTwo from '../../../components/ChartTwo';
@@ -13,12 +13,27 @@ import CustomInput from '../../../common/input';
 import { useMutation } from 'react-query';
 import { createUser } from '../../../api/createUserApi';
 import apiClient from '../../../api/apiClient';
+import Table from '../../../components/Table/index';
 import axios from 'axios';
 import { displayErrorMessage, displaySuccessMessage } from '../../../components/toast/Toast';
 
 const AdminDashboard = () => {
     const [size, setSize] = useState<SizeType>('large'); // default is 'middle'
     const [modalOpen, setModalOpen] = useState(false);
+
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+      fetchUsers()
+    }, [])
+    const fetchUsers = async () => {
+      try {
+        const { data } = await axios.get('http://192.168.0.157/clims/public/api/users')
+        setUsers(data)
+      } catch (error) {
+        displayErrorMessage("An error occured try again later")
+      }
+    }
+  
 
     const cardData = [
         {
@@ -134,6 +149,10 @@ const AdminDashboard = () => {
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
                     {cardData.map(({ amount, text, percentage, id }) => <CustomCard key={id} amount={amount} text={text} percentage={percentage} currency={''} />)}
                 </div>
+
+                <div className="col-span-12 xl:col-span-8 mt-10" >
+      <Table data={users}/>
+        </div>
 
                 <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
                     {/* <ChartOne />
