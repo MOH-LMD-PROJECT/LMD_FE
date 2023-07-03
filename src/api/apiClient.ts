83c11f58605cb4171a2b61a7803ca680e831fbed
Bkroland19 <bkroland19@gmail.com>
2023-07-03 14:24:51 +0300

    react query setup

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,20 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
- const apiClient = axios.create({
-	baseURL: "https://covid19.gou.go.ug/clims_backend/public/api",
+const apiClient = axios.create({
+  baseURL: 'https://covid19.gou.go.ug/clims_backend/public/api',
 });
 
-
-apiClient.defaults.headers.post["Content-Type"] = "application/json";
+apiClient.defaults.headers.post['Content-Type'] = 'application/json';
 apiClient.interceptors.request.use(
-	( config ) => {
-		// const token = state.accessToken;
-		// const auth = token ? `Bearer ${token}` : '';
-		// config.headers.common['Authorization'] = auth;
-		return config;
-	},
-	( error ) => Promise.reject( error )
+  (config) => {
+    // const token = state.accessToken;
+    // const auth = token ? `Bearer ${token}` : '';
+    // config.headers.common['Authorization'] = auth;
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
-
-export default apiClient
+export default apiClient;
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './satoshi.css';
-import { store, persistor } from './redux/store'
 import { Provider } from 'react-redux'
-import { ReactQueryDevtools } from 'react-query/devtools'
-import { PersistGate } from 'redux-persist/es/integration/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { PersistGate } from 'redux-persist/es/integration/react';
+import { store } from './redux/store';
 
 
 const queryClient = new QueryClient({
@@ -20,17 +19,15 @@ const queryClient = new QueryClient({
   },
 })
 
-persistor.purge()
+// persistor.purge()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
       <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <App />
-            <ReactQueryDevtools initialIsOpen={false} />
-          </PersistGate>
+        <Provider store={store} >
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
         </Provider>
       </QueryClientProvider>
     </Router>
