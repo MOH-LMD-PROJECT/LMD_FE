0d626bed3b9dc90a1cb622bd1426bbcda46c024a
Bkroland19 <bkroland19@gmail.com>
2023-07-05 18:16:28 +0300

    deployment errors

--- src/pages/Tables.tsx
+++ src/pages/Tables.tsx
@@ -1,4 +1,4 @@
-import Breadcrumb from '../../components/Breadcrumb';
+// import Breadcrumb from '../../components/Breadcrumb';
 import TableOne from '../../components/TableOne';
 import TableThree from '../../components/TableThree';
 import TableTwo from '../../components/TableTwo';
@@ -6,7 +6,7 @@ import TableTwo from '../../components/TableTwo';
 const Tables = () => {
   return (
     <>
-      <Breadcrumb pageName="Tables" />
+      {/* <Breadcrumb pageName="Tables" /> */}
 
       <div className="flex flex-col gap-10">
         <TableOne />
