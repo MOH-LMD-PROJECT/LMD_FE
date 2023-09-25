d613f73e452ca528d82d58c3438371c1e472d332
engWafula <wafulaallan5@gmail.com>
2023-09-25 20:41:41 +0300

    adding user management

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -230,6 +230,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               procured
                             </NavLink>
                           </li>
+                          <li>
+                            <NavLink
+                              to="/moh/admin/usermanagment"
+                              className={({ isActive }) =>
+                                'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
+                                (isActive && '!text-white')
+                              }
+                            >
+                              User Managment
+                            </NavLink>
+                          </li>
                         </ul>
                       </div>
                       {/* <!-- Dropdown Menu End --> */}
